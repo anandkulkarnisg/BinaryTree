2bda23f3587f48640120c57c6d15c99d9ec36f27
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2022-06-28 12:26:51 +0800

    Anand Kulkarni - 28/06/2022 12:26:51:159 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- bst/Example7.cpp
+++ bst/Example7.cpp
@@ -8,12 +8,12 @@ int main(int argc, char* argv[])
 {
   BinarySearchTree<int> bstTree(false);
   for(const auto& item : {15,10,20,8,12,18,16,19,30})
-    bstTree.insertItem(item); 
+    bstTree.insertItem(item);
   bstTree.doInOrderTraversal();
   cout<<"Case 3 : Attempting to delete root node = 15"<<endl;
   bstTree.deleteItem(15);
   bstTree.doInOrderTraversal();
-  cout<<"the root node now is ="<<bstTree.getRoot()->m_data<<endl; 
+  cout<<"the root node now is = "<<bstTree.getRoot()->m_data<<endl;
   // Now switch the policy recreate the same bstTree and use predecessor policy for deletion.
   bstTree.reset();  // Clean the tree.
   bstTree.setBSTRebalancePolicy(bstRebalancePolicy::predecessor);
--- bst/Example8.cpp
+++ bst/Example8.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<BinarySearchTree.h>
+#include<magic_enum.hpp>
 
 using namespace std;
 
@@ -8,26 +9,21 @@ int main(int argc, char* argv[])
 {
   BinarySearchTree<int> bstTree(false);
   for(const auto& item : {15,10,20,8,12,18,16,19,30})
-    bstTree.insertItem(item); 
+    bstTree.insertItem(item);
   bstTree.doInOrderTraversal();
-  cout<<"Case 1 : Aiming for a root node deletion(15) with successor policy"<<endl;
-  bstTree.deleteItem(15);
-  bstTree.doInOrderTraversal();
-  cout<<"After the root removal new root is = "<<bstTree.getRoot()->m_data<<endl;
-  cout<<"Case 2 : Aiming for a root node deletion(16) with predecessor policy"<<endl;
-  bstTree.setBSTRebalancePolicy(bstRebalancePolicy::predecessor);
-  bstTree.deleteItem(16);
-  bstTree.doInOrderTraversal();
-  cout<<"After the root removal new root is = "<<bstTree.getRoot()->m_data<<endl;
-  cout<<"Case 3 : Aiming for a root node deletion(12) with height policy"<<endl;
-  bstTree.setBSTRebalancePolicy(bstRebalancePolicy::height);
-  bstTree.deleteItem(12);
-  bstTree.doInOrderTraversal();
-  cout<<"After the root removal new root is = "<<bstTree.getRoot()->m_data<<endl;
-  cout<<"Case 4 : Aiming for a root node deletion(18) with size policy"<<endl;
-  bstTree.setBSTRebalancePolicy(bstRebalancePolicy::size);
-  bstTree.deleteItem(18);
-  bstTree.doInOrderTraversal();
-  cout<<"After the root removal new root is = "<<bstTree.getRoot()->m_data<<endl;
+  vector<bstRebalancePolicy> policyVec={bstRebalancePolicy::successor,
+    bstRebalancePolicy::predecessor,
+    bstRebalancePolicy::height,
+    bstRebalancePolicy::size
+  };
+  vector<int> itemsToBeDeleted={15, 16, 12, 18};
+  for(size_t i=0;i<itemsToBeDeleted.size();++i){
+    bstTree.setBSTRebalancePolicy(policyVec[i]);
+    cout<<"Case "<<i<<" : Aiming for a root node deletion ("<<itemsToBeDeleted[i]<<") with ";
+    cout<<magic_enum::enum_name(bstTree.getBSTRebalancePolicy())<<" policy"<<endl;
+    bstTree.deleteItem(itemsToBeDeleted[i]);
+    bstTree.doInOrderTraversal();
+    cout<<"After the root removal new root is = "<<bstTree.getRoot()->m_data<<endl;
+  }
   return(0);
 }
