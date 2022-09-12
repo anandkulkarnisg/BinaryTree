fda8744995e5b959e619295521ffa0b94005c9bf
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2022-09-12 21:17:00 +0800

    Anand Kulkarni - 12/09/2022 21:16:58:185 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- set5/sumodduncle.cpp
+++ set5/sumodduncle.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 using namespace boost;
 
+
+// Solution 1.
 void findSumImpl(const BinaryTreeNode<int>* node, std::vector<std::pair<const BinaryTreeNode<int>*, bool>>& arr, const int& idx, long& sum, const bool childType){
   if(!node)
     return;
@@ -30,12 +32,35 @@ void findSumImpl(const BinaryTreeNode<int>* node, std::vector<std::pair<const Bi
   findSumImpl(node->m_rightChild, arr, idx+1, sum, false);
 }
 
-long findSumwhereGrandParentChildrenOdd(const unique_ptr<BinaryTree<int>>& treeRef){
+long findSumWhereUncleAndNodeAreOdd(const unique_ptr<BinaryTree<int>>& treeRef){
   long sum=0;
   size_t height=treeRef.get()->height();
   std::vector<std::pair<const BinaryTreeNode<int>* ,bool>> arr;
   arr.reserve(height+1);
   findSumImpl(treeRef.get()->getRoot(), arr, 0, sum, false);
+  return(sum);
+}
+
+
+void findSumImpl2(const BinaryTreeNode<int>* node, const BinaryTreeNode<int>* parentNode, const size_t currentLevel, long & sum, const bool isLeftOrRight){
+  if(!node)
+    return;
+  if(currentLevel>0&&parentNode){
+    BinaryTreeNode<int>* uncleNode=(isLeftOrRight)?parentNode->m_rightChild:parentNode->m_leftChild;
+    if(node->m_leftChild&&node->m_leftChild->m_data%2==1&&uncleNode&&uncleNode->m_data%2==1)
+      sum+=node->m_leftChild->m_data; 
+    if(node->m_rightChild&&node->m_rightChild->m_data%2==1&&uncleNode&&uncleNode->m_data%2==1)
+      sum+=node->m_rightChild->m_data;
+  }
+  // Now invoke further recursive calls.
+  findSumImpl2(node->m_leftChild, node, currentLevel+1, sum, true);
+  findSumImpl2(node->m_rightChild, node, currentLevel+1, sum, false);
+}
+
+// Solution 2 : The auxillary complexity is now O(1) as we dont use array of size h or is it really (since stack eventually holds all recursive call variables in until it unwinds )?
+long findSumOddUncle(const unique_ptr<BinaryTree<int>>& treeRef){
+  long sum=0;
+  findSumImpl2(treeRef.get()->getRoot(), nullptr, 0, sum, false);
   return(sum);
 }
 
@@ -51,9 +76,11 @@ long findSumwhereGrandParentChildrenOdd(const unique_ptr<BinaryTree<int>>& treeR
 // Q:Is it possible to make auxillary memory O(1) to keep track of only parent and grand parent for each node ?
 
 int main(int argc, char* argv[]){
-  //unique_ptr<BinaryTree<int>> uniqueBstPtr(new BinaryTree<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
-  unique_ptr<BinaryTree<int>> uniqueBstPtr(new BinaryTree<int>({1, 2, 3, 4, 5, 6, 7}));
-  long sum=findSumwhereGrandParentChildrenOdd(uniqueBstPtr);
+  unique_ptr<BinaryTree<int>> uniqueBstPtr(new BinaryTree<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
+  //unique_ptr<BinaryTree<int>> uniqueBstPtr(new BinaryTree<int>({1, 2, 3, 4, 5, 6, 7}));
+  long sum=findSumWhereUncleAndNodeAreOdd(uniqueBstPtr);
+  long sumAlt=findSumOddUncle(uniqueBstPtr);
   cout<<"sum="<<sum<<endl;
+  cout<<"sumAlt="<<sumAlt<<endl;
   return(0);
 }
